7a37a0dae54572114f8baa148e7afd907340ce13
Ankur Mittal <mittankur1999@gmail.com>
2021-03-25 02:05:10 +0530

    minor fix

--- src/DatasetView.js
+++ src/DatasetView.js
@@ -92,7 +92,7 @@ async function handleOpenAndReadFiles() {
     // });
     // console.log(blobs)
     // let tileData = [];
-    
+
     // let name_list = blobs.map(item => item["name"])
     // tileData = name_list.map(item => {
     //     return {
@@ -104,11 +104,11 @@ async function handleOpenAndReadFiles() {
     // for (let index = 0; index < blobs.length; ++index) {
     //     const reader = new FileReader();
     //     reader.onload = event =>{
-            
+
     //         tileData.push({src: event.target.result, alt: blobs[index].name})
     //         name_list.push(blobs[index].name)
     //     }
-        
+
     //     reader.readAsDataURL(blobs[index]);
     // }
     if (!main_socket.connected) {
@@ -119,7 +119,7 @@ async function handleOpenAndReadFiles() {
     })
     // masterTile[current_class.toString()] = tileData
     // setTimeout(5000)
-    return {data: [], numFiles: []};   
+    return {data: [], numFiles: []};
 }
 
 function handleSelectFiles(setTileData) {
@@ -161,7 +161,7 @@ function handlePreviewImages(tileData) {
             }
             if (main_socket.connected) {
                 console.log("ready to send");
-                main_socket.emit("apply_operations", {"images":name_list, 
+                main_socket.emit("apply_operations", {"images":name_list,
                                                     "operations": manipulation_data,
                                                     "class":current_class});
             }
@@ -196,51 +196,9 @@ function ManipulationInputOptions({setTileData}) {
                                 }
                             }}
                         >
-                            <MenuItem value={0}>0</MenuItem>
-                            <MenuItem value={1}>1</MenuItem>
-                            <MenuItem value={2}>2</MenuItem>
-                            <MenuItem value={3}>3</MenuItem>
-                            <MenuItem value={4}>4</MenuItem>
-                            <MenuItem value={5}>5</MenuItem>
-                            <MenuItem value={6}>6</MenuItem>
-                            <MenuItem value={7}>7</MenuItem>
-                            <MenuItem value={8}>8</MenuItem>
-                            <MenuItem value={9}>9</MenuItem>
-                            <MenuItem value={10}>10</MenuItem>
-                            <MenuItem value={11}>11</MenuItem>
-                            <MenuItem value={12}>12</MenuItem>
-                            <MenuItem value={13}>13</MenuItem>
-                            <MenuItem value={14}>14</MenuItem>
-                            <MenuItem value={15}>15</MenuItem>
-                            <MenuItem value={16}>16</MenuItem>
-                            <MenuItem value={17}>17</MenuItem>
-                            <MenuItem value={18}>18</MenuItem>
-                            <MenuItem value={19}>19</MenuItem>
-                            <MenuItem value={20}>20</MenuItem>
-                            <MenuItem value={21}>21</MenuItem>
-                            <MenuItem value={22}>22</MenuItem>
-                            <MenuItem value={23}>23</MenuItem>
-                            <MenuItem value={24}>24</MenuItem>
-                            <MenuItem value={25}>25</MenuItem>
-                            <MenuItem value={26}>26</MenuItem>
-                            <MenuItem value={27}>27</MenuItem>
-                            <MenuItem value={28}>28</MenuItem>
-                            <MenuItem value={29}>29</MenuItem>
-                            <MenuItem value={30}>30</MenuItem>
-                            <MenuItem value={31}>31</MenuItem>
-                            <MenuItem value={32}>32</MenuItem>
-                            <MenuItem value={33}>33</MenuItem>
-                            <MenuItem value={34}>34</MenuItem>
-                            <MenuItem value={35}>35</MenuItem>
-                            <MenuItem value={36}>36</MenuItem>
-                            <MenuItem value={37}>37</MenuItem>
-                            <MenuItem value={38}>38</MenuItem>
-                            <MenuItem value={39}>39</MenuItem>
-                            <MenuItem value={40}>40</MenuItem>
-                            <MenuItem value={41}>41</MenuItem>
-                            <MenuItem value={42}>42</MenuItem>
-                            <MenuItem value={43}>43</MenuItem>
-                            <MenuItem value={44}>44</MenuItem>
+                            {Array(45).fill(0).map((temp, index) =>
+                                <MenuItem value={index} key={index}>{index}</MenuItem>
+                            )}
                         </Select>
                     </FormControl>
                 </Grid>
