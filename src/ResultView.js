0ab7bbdc7a71c2d9b0ecda6b9063008bff60e8d0
Ankur Mittal <mittankur1999@gmail.com>
2021-03-25 22:02:08 +0530

    read csv

--- src/ResultView.js
+++ src/ResultView.js
@@ -1,34 +1,29 @@
 import React from "react";
 import {Paper, Button} from "@material-ui/core";
+import CSVReader from "react-csv-reader";
 import {io} from "socket.io-client";
 import {backendURL} from "./backendConfig";
 
 // Starting Main Socket connection it will be restarted if it is disconnected and it is needed
 let main_socket = io(backendURL, {transports: ['websocket'], upgrade: false});
 
-async function csv_opener(){
-    if (!main_socket.connected) {
-        main_socket.connect()
-    }
-    main_socket.emit("result_setup")
-}
-
 function ResultView() {
     if (!main_socket.connected) {
         main_socket.connect()
     }
-    main_socket.on("result_array", data => {
-        console.log(data)
-    })
+
     return (
         <React.Fragment>
             <Paper variant="outlined">
-                <Button onClick={()=>{
-                    csv_opener()
-                }}>
-                    Name
-                </Button>
-
+                <span style={{display: "none"}}>
+                    <CSVReader inputId="select-csv-input" onFileLoaded={(data, fileInfo) => {
+                        console.log(data, fileInfo);
+                        main_socket.emit("csv_data", {"data": data, "fileInfo": fileInfo});
+                    }}/>
+                </span>
+                <Button onClick={() => {
+                    document.getElementById("select-csv-input").click();
+                }}>Select CSV</Button>
             </Paper>
         </React.Fragment>
     );
